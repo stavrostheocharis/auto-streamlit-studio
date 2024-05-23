45ee794f41fe9df136f76d1d21a6153a55c241c7
Stavros Theocharis <stavrostheocharis@yahoo.gr>
2024-05-23 23:56:10 +0200

    Additions

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import os
-from chat import chat
-from utils import get_script
+from utils import get_script, generate_arctic_response
 
 # App title
-st.set_page_config(page_title="Test", page_icon="🤖", layout="wide")
+st.set_page_config(page_title="Auto-streamlit", page_icon="🤖", layout="wide")
+
+if "rerun" not in st.session_state.keys():
+    st.session_state.rerun = False
 
 
-# st.title("UPLine AI")
 def display_sidebar_ui():
     with st.sidebar:
-        st.title("Test")
+        st.title("Auto-streamlit")
 
         if "REPLICATE_API_TOKEN" in st.secrets:
             print("Replicate API token found in secrets.")
@@ -26,18 +27,46 @@ def display_sidebar_ui():
         os.environ["REPLICATE_API_TOKEN"] = replicate_api
 
 
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Hi., I'm your personal Streamlit assistant, a new intelligent assistant to help you create automatically streamlit apps! What do you want to build?",
+        }
+    ]
+
 display_sidebar_ui()
 with st.sidebar:
-    chat()
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user", avatar="⛷️"):
+            st.write(prompt)
 
-get_script(st.session_state.messages[-1])
+    # Generate a new response if last message is not from assistant
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            response = generate_arctic_response()
+            full_response = st.write_stream(response)
+        message = {"role": "assistant", "content": full_response}
+        st.session_state.messages.append(message)
+
+        created_file = get_script(st.session_state.messages[-1])
+        if created_file:
+            st.session_state.rerun = True
+            st.rerun()
+
 
 file_path = "streamlit_app.py"
 
+print(st.session_state.messages, " st.session_state.messages")
 if os.path.exists(file_path):
     from streamlit_app import main
 
     main()
-    st.rerun()
+
+    # print(st.session_state.messages)
+    if st.session_state.rerun:
+        st.session_state.rerun = False
+        st.rerun()
 else:
     print(f"The file {file_path} does not exist.")
--- utils.py
+++ utils.py
@@ -1,31 +1,37 @@
 import streamlit as st
 import replicate
 from transformers import AutoTokenizer
-import json
 import re
 
 
-def get_script(response):
-
-    # Extract the content from the response
-    content = response["content"]
-    print("Content:\n", content)
+def extract_code_from_answer(content):
     # Use regular expression to find the code block
 
     code_block = re.search(r"```python\s*(.*?)\s*```", content, re.DOTALL) or re.search(
         r"```python\s*(.*)", content, re.DOTALL
     )
+    if code_block:
+        return code_block
+    else:
+        code_block = re.search(r"```\s*(.*?)\s*```", content, re.DOTALL) or re.search(
+            r"```\s*(.*)", content, re.DOTALL
+        )
+    return code_block
 
+
+def get_script(response):
+    code_block = extract_code_from_answer(response["content"])
     if code_block:
         code = code_block.group(1)
 
         # Write the code to a .py file
         with open("streamlit_app.py", "w") as file:
             file.write(code)
-
         print("Code extracted and written to streamlit_app.py")
+        return True
     else:
         print("No code block found in the response.")
+        return False
 
 
 @st.cache_data(show_spinner=False)
@@ -52,22 +58,41 @@ def clear_chat_history():
     st.session_state.chat_aborted = False
 
 
-def analyze_query(query):
-    """Analyze the user's query to understand the context and intent."""
-    system_prompt = (
-        "Analyze the following user query and provide a more detailed and contextually rich version of how the streamlit python code can be created in steps. "
-        "Do not create code at this phase only create a plan of how the app will look like.  "
-        "Keep as granted that streamlit is already installed \n\n"
+def correct_code(query):
+    """Analyze the user's code to correct it."""
+    prompt = []
+    system_prompt_content = (
+        "You are a python code corrector. "
+        "Answer only by providing the code. "
+        "Do not explain what the app and code do. "
+        "Analyze the following streamlit code part and provide a validated code that can run. "
+        "Make sure that a def main(): part of function exists. "
+        "Validate that the decorator  @st.experimental_fragment and not another decorator exists on top of the def main(): part \n\n"
     )
+    # system_prompt = "<|im_start|>system\n" + system_prompt_content + "<|im_end|>"
+    system_prompt = (
+        "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+        + system_prompt_content
+        + "<|eot_id|>"
+    )
+    prompt.append(system_prompt)
 
-    full_prompt = system_prompt + query
+    # query_prompt = "<|im_start|>user\n" + query + "<|im_end|>"
+    query_prompt = "<|start_header_id|>user<|end_header_id|>\n\n" + query + "<|eot_id|>"
+    prompt.append(query_prompt)
+
+    # prompt.append("<|im_start|>assistant")
+    prompt.append("<|start_header_id|>assistant<|end_header_id|>\n\n")
+    prompt.append("")
+    prompt_str = "\n".join(prompt)
 
     response = ""
     for event in replicate.stream(
-        "snowflake/snowflake-arctic-instruct",
+        "meta/meta-llama-3-70b-instruct",
         input={
-            "prompt": full_prompt,
-            "temperature": 0.5,
+            "prompt": prompt_str,
+            "prompt_template": r"{prompt}",
+            "temperature": 0.2,
             "top_p": 0.9,
         },
     ):
@@ -76,46 +101,65 @@ def analyze_query(query):
     return response.strip()
 
 
-# Function for generating Snowflake Arctic response
-def generate_arctic_response(prompt_str, temperature, top_p):
-    # Check if the prompt exceeds the token limit
+system_prompt = (
+    "You are an experienced python software engineer. "
+    "Answer only by providing the code. "
+    "Do not explain what the app and code do. "
+    "The app has to be created with streamlit library. "
+    "The app has to be created with a def main(): function "
+    # "In the previous like of the the main() function you have always to include the decorator: @st.experimental_fragment"
+    "The code you produce should be able to run by itself and do not leave parts to be added later by user or other code parts. "
+    "You should not try to open files that you are not sure if they exist in a path. "
+    "When you create widgets you have to inlcude each time a different unique `key` for each one of them. "
+    "Use the following pieces of context and provide only the python code: \n\n"
+)
 
-    system_prompt = (
-        "You are an experienced python software engineer. "
-        "Answer only by providing the code. "
-        # "Answer only by providing the code no explanations, details, or other text. "
-        "Do not explain what the app and code do. "
-        "The app has to be created with streamlit library. "
-        "The app has to be created with a def main(): function "
-        "The code you produce should be able to run by itself and do not leave parts to be added later by user or other code parts. "
-        "You should not try to open files that you are not sure if they exist in a path. "
-        "When you create widgets you have to inlcude each time a different unique `key` for each one of them. "
-        "Use the following pieces of context and provide only the python code: \n\n"
-    )
-    full_prompt = system_prompt + prompt_str
-    print("The full prompt is: ", full_prompt)
-    if get_num_tokens(prompt_str) > 2048:
-        st.error("Conversation length too long. Please keep it under 3072 tokens.")
+system_prompt = (
+    "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+    + system_prompt
+    + "<|eot_id|>"
+)
+# system_prompt = "<|im_start|>system\n" + system_prompt + "<|im_end|>"
+
+
+def generate_arctic_response():
+    prompt = [system_prompt]
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            # prompt.append("<|im_start|>user\n" + dict_message["content"] + "<|im_end|>")
+            prompt.append(
+                "<|start_header_id|>user<|end_header_id|>\n\n"
+                + dict_message["content"]
+                + "<|eot_id|>"
+            )
+        else:
+            prompt.append(
+                "<|start_header_id|>assistant<|end_header_id|>\n\n"
+                + dict_message["content"]
+                + "<|eot_id|>"
+            )
+            # prompt.append(
+            #     "<|im_start|>assistant\n" + dict_message["content"] + "<|im_end|>"
+            # )
+    # prompt.append("<|im_start|>assistant")
+    prompt.append("<|start_header_id|>assistant<|end_header_id|>")
+    prompt.append("")
+    prompt_str = "\n".join(prompt)
+
+    if get_num_tokens(prompt_str) >= 4096:
+        st.error("Conversation length too long. Please keep it under 4096 tokens.")
         st.button(
             "Clear chat history", on_click=clear_chat_history, key="clear_chat_history"
         )
         st.stop()
 
-    # response = ""
-    st.session_state.messages.append({"role": "assistant", "content": ""})
-    for _, event in enumerate(
-        replicate.stream(
-            "snowflake/snowflake-arctic-instruct",
-            input={
-                "prompt": full_prompt,
-                "prompt_template": r"{prompt}",
-                "temperature": temperature,
-                "top_p": top_p,
-            },
-        )
+    for event in replicate.stream(
+        "meta/meta-llama-3-70b-instruct",
+        input={
+            "prompt": prompt_str,
+            "prompt_template": r"{prompt}",
+            "temperature": 0.2,
+            "top_p": 0.9,
+        },
     ):
-        st.session_state.messages[-1]["content"] += str(event)
-        # response += str(event)
-        # response_container.markdown(response)  # Update the response incrementally
-        # return response
         yield str(event)
