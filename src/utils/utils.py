21784eb40b66414102863e60d545262739fa3c51
Stavros Theocharis <stavrostheocharis@yahoo.gr>
2024-06-28 11:28:59 +0300

    Adjustments
     * Added streamlit config for palette
     * Fixed baloons
     * Added toast messages in save
     * Added utils for info and logo

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -31,3 +31,34 @@ def clear_chat_history():
         }
     ]
     st.session_state.chat_aborted = False
+
+
+@st.cache_data(ttl=3600)
+def add_logo():
+    st.markdown(
+        f"""
+            <style>
+                [data-testid="stSidebar"] {{
+                    background-image: url(https://www.uphellas.gr/_next/static/media/logo.b4285350.svg);
+                    background-repeat: no-repeat;
+                    padding-top: 80px;
+                    background-position: 20px 20px;
+                }}
+            </style>
+            """,
+        unsafe_allow_html=True,
+    )
+
+
+@st.cache_data(ttl=3600)
+def display_links(repo_link, other_link) -> None:
+    """Displays a repository and other link"""
+    col1, col2 = st.sidebar.columns(2)
+    col1.markdown(
+        f"<a style='display: block; text-align: center;' href={repo_link}>Source code</a>",
+        unsafe_allow_html=True,
+    )
+    col2.markdown(
+        f"<a style='display: block; text-align: center;' href={other_link}>App introduction</a>",
+        unsafe_allow_html=True,
+    )
